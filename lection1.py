507cf5d0ee822aaeb238adad0732d8ac9fd4b7b3
Tatiana <www.t97@mail.ru>
2022-09-06 13:28:31 +0300

    добавлены задачи по 1 занятию

---
+++ lection1.py
@@ -0,0 +1,15 @@
+print('hello world')
+
+a = 123
+b = 1.23
+c = 'hello world'
+d = "hello 'world'"
+print(a, b, c)
+print(a, '-', b, '-', c)
+#или print('{1} - {2} - {0}'.format(a, b, c))
+#или print(f'{a} - {b} - {c}')
+
+print('введите e')
+e = input()
+
+# комментарий
